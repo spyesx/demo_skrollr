e262968c12408c4b3a7086215e4c1db245ca6728
Nicolas Bages <nicolas.bages@weinto.com>
2014-06-10 21:05:13 +0200

    [card_effect] WIP parallax slides + diamond

--- card_effect.php
+++ card_effect.php
@@ -25,7 +25,10 @@
 
 		return $r;
 	}
-	$pos = slidesPositions(4, 1800, 1000, 800);
+	$pos                = slidesPositions(7, 1800, 2000, 800);
+	$framePosFrom       = 800;
+	$framePosTo         = $pos['slide-6']['transition'];
+	$framePosTransition = $framePosTo + 600;
 
 ?>
 
@@ -35,9 +38,9 @@
 
 		<div id="frame"
 			data-start="top:2000px;" 
-	        data-800-start="top:900px" 
-	        data-7800-start="top:7900px" 
-	        data-8200-start="top:7500px"
+	        data-<?php echo $framePosFrom; ?>-start="top:900px" 
+	        data-<?php echo $framePosTo; ?>-start="top:<?php echo $framePosTo + 100; ?>px" 
+	        data-<?php echo $framePosTransition; ?>-start="top:<?php echo $framePosTo - 200; ?>px"
 			>
 			<div id="frame-slider">
 				<div class="slide slide-1"
@@ -53,38 +56,76 @@
 					data-anchor-target="#container"
 					></div>
 				<div class="slide slide-3"
-					data-start="top:0px;" 
 					data-<?php echo $pos['slide-3']['from']; ?>-start="top:0px" 
 					data-<?php echo $pos['slide-3']['to']; ?>-start="top:0px" 
 					data-<?php echo $pos['slide-3']['transition']; ?>-start="top:-400px" 
 					data-anchor-target="#container"
 					></div>
 				<div class="slide slide-4"
-					data-start="top:0px;" 
 					data-<?php echo $pos['slide-4']['from']; ?>-start="top:0px" 
 					data-<?php echo $pos['slide-4']['to']; ?>-start="top:0px" 
 					data-<?php echo $pos['slide-4']['transition']; ?>-start="top:-400px" 
 					data-anchor-target="#container"
 					></div>
 				<div class="slide slide-5"
-					data-start="top:0px;" 
-					data-3400-start="top:0px" 
-					data-3800-start="top:-400px" 
+					data-<?php echo $pos['slide-5']['from']; ?>-start="top:0px" 
+					data-<?php echo $pos['slide-5']['to']; ?>-start="top:0px" 
+					data-<?php echo $pos['slide-5']['transition']; ?>-start="top:-400px" 
 					data-anchor-target="#container"
 					></div>
 				<div class="slide slide-6"></div>
 			</div>
 		</div>
 
-		<div id="caption" class="diamond diamond-red">
+		<div id="caption" class="diamond diamond-red"
+			data-start="top:2000px;" 
+	        data-<?php echo $framePosFrom+300; ?>-start="top:1500px" 
+	        data-<?php echo $framePosTo; ?>-start="top:<?php echo $framePosTo + 300; ?>px" 
+	        data-<?php echo $framePosTransition; ?>-start="top:<?php echo $framePosTo - 200; ?>px"
+			>
 			<div>
 				<ul>
-					<li>Slide 1</li>
-					<li>Slide 2</li>
-					<li>Slide 3</li>
-					<li>Slide 4</li>
-					<li>Slide 5</li>
-					<li>Slide 6</li>
+					<li
+						data-<?php echo $pos['slide-1']['from']-100; ?>-start="opacity:0" 
+						data-<?php echo $pos['slide-1']['from']; ?>-start="opacity:1" 
+						data-<?php echo $pos['slide-1']['to']; ?>-start="opacity:1" 
+						data-<?php echo $pos['slide-1']['transition']; ?>-start="opacity:0" 
+						data-anchor-target="#container"
+						><span>Slide 1</span></li>
+					<li
+						data-<?php echo $pos['slide-2']['from']-100; ?>-start="opacity:0" 
+						data-<?php echo $pos['slide-2']['from']; ?>-start="opacity:1" 
+						data-<?php echo $pos['slide-2']['to']; ?>-start="opacity:1" 
+						data-<?php echo $pos['slide-2']['transition']; ?>-start="opacity:0" 
+						data-anchor-target="#container"
+						><span>Slide 2</span></li>
+					<li
+						data-<?php echo $pos['slide-3']['from']-100; ?>-start="opacity:0" 
+						data-<?php echo $pos['slide-3']['from']; ?>-start="opacity:1" 
+						data-<?php echo $pos['slide-3']['to']; ?>-start="opacity:1" 
+						data-<?php echo $pos['slide-3']['transition']; ?>-start="opacity:0" 
+						data-anchor-target="#container"
+						><span>Slide 3</span></li>
+					<li
+						data-<?php echo $pos['slide-4']['from']-100; ?>-start="opacity:0" 
+						data-<?php echo $pos['slide-4']['from']; ?>-start="opacity:1" 
+						data-<?php echo $pos['slide-4']['to']; ?>-start="opacity:1" 
+						data-<?php echo $pos['slide-4']['transition']; ?>-start="opacity:0" 
+						data-anchor-target="#container"
+						><span>Slide 4</span></li>
+					<li
+						data-<?php echo $pos['slide-5']['from']-100; ?>-start="opacity:0" 
+						data-<?php echo $pos['slide-5']['from']; ?>-start="opacity:1" 
+						data-<?php echo $pos['slide-5']['to']; ?>-start="opacity:1" 
+						data-<?php echo $pos['slide-5']['transition']; ?>-start="opacity:0" 
+						data-anchor-target="#container"
+						><span>Slide 5</span></li>
+					<li
+						data-<?php echo $pos['slide-6']['from']-100; ?>-start="opacity:0" 
+						data-<?php echo $pos['slide-6']['to']; ?>-start="opacity:1" 
+						data-<?php echo $pos['slide-6']['transition']; ?>-start="opacity:0" 
+						data-anchor-target="#container"
+						><span>Slide 6</span></li>
 				</ul>
 			</div>
 		</div>
